4364eae7e9b2a1b25f10dffb16d7fafd1a6403d6
SEFAKHARINMOV <Kharimovfshafa@gmail.com>
2024-05-22 16:21:15 +0400

    LeetCode

---
+++ src/Leetcode_704_E/Solution.java
@@ -0,0 +1,21 @@
+package Leetcode_704_E;
+
+public class Solution {
+        public int search ( int[] nums, int target){
+            int left = 0;
+             int right = nums.length ;
+            while (left <  right) {
+                int mid = left + (left - right) / 2;
+                if (nums[mid] == target){
+                    return mid;
+                } else if (nums[mid]< target ) {
+                    left = mid + 1;
+                    
+                }else {
+                    right = mid;
+                }
+            }
+            return -1;
+        }
+    }
+
